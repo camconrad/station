b5d0a17b4471b611c0c159b9b930fa303a1e84b7
camconrad <camconrad@gmail.com>
2024-10-20 16:36:02 -0700

    build

--- test/Lock.ts
+++ test/Lock.ts
@@ -1,4 +1,4 @@
-import { loadFixture, ethers } from "hardhat";
+import { ethers } from "hardhat";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 
@@ -7,38 +7,42 @@ async function increaseTime(seconds: number) {
   await ethers.provider.send("evm_mine");
 }
 
+async function deployOneYearLockFixture() {
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const ONE_GWEI = 1_000_000_000;
+
+  const lockedAmount = ONE_GWEI;
+  const latestBlock = await ethers.provider.getBlock("latest");
+  const unlockTime = latestBlock.timestamp + ONE_YEAR_IN_SECS;
+
+  const [owner, otherAccount] = await ethers.getSigners();
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  return { lock, unlockTime, lockedAmount, owner, otherAccount };
+}
+
 describe("Lock", function () {
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const latestBlock = await ethers.provider.getBlock("latest");
-    const unlockTime = latestBlock.timestamp + ONE_YEAR_IN_SECS;
-
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+  beforeEach(async function () {
+    this.fixture = await deployOneYearLockFixture();
+  });
 
   describe("Deployment", function () {
     it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+      const { lock, unlockTime } = this.fixture;
 
       expect(await lock.unlockTime()).to.equal(unlockTime);
     });
 
     it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+      const { lock, owner } = this.fixture;
 
       expect(await lock.owner()).to.equal(owner.address);
     });
 
     it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+      const { lock, lockedAmount } = this.fixture;
 
       expect(await ethers.provider.getBalance(lock.address)).to.equal(lockedAmount);
     });
@@ -55,13 +59,13 @@ describe("Lock", function () {
   describe("Withdrawals", function () {
     describe("Validations", function () {
       it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+        const { lock } = this.fixture;
 
         await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
       });
 
       it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(deployOneYearLockFixture);
+        const { lock, unlockTime, otherAccount } = this.fixture;
 
         await increaseTime(unlockTime - (await ethers.provider.getBlock("latest")).timestamp);
 
@@ -71,7 +75,7 @@ describe("Lock", function () {
       });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+        const { lock, unlockTime } = this.fixture;
 
         await increaseTime(unlockTime - (await ethers.provider.getBlock("latest")).timestamp);
 
@@ -81,7 +85,7 @@ describe("Lock", function () {
 
     describe("Events", function () {
       it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(deployOneYearLockFixture);
+        const { lock, unlockTime, lockedAmount } = this.fixture;
 
         await increaseTime(unlockTime - (await ethers.provider.getBlock("latest")).timestamp);
 
@@ -93,7 +97,7 @@ describe("Lock", function () {
 
     describe("Transfers", function () {
       it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(deployOneYearLockFixture);
+        const { lock, unlockTime, lockedAmount, owner } = this.fixture;
 
         await increaseTime(unlockTime - (await ethers.provider.getBlock("latest")).timestamp);
 
